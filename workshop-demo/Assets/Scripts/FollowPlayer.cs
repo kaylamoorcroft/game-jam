cd2a8987cbb73916b8993b34e9603774f575dd6e
kaylamoorcroft <kayla.moorcroft@icloud.com>
2024-03-22 18:18:33 -0300

    Deleted enemies in scene, renamed scene, change camera movement for following player

--- workshop-demo/Assets/Scripts/FollowPlayer.cs
+++ workshop-demo/Assets/Scripts/FollowPlayer.cs
@@ -7,6 +7,7 @@ public class FollowPlayer : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private float offsetX;
     [SerializeField] private float offsetY;
+    [SerializeField] private float smoothing = 1f; // controls how smooth camera moves
     private Vector2 playerPosition;
 
     // Start is called before the first frame update
@@ -24,7 +25,14 @@ public class FollowPlayer : MonoBehaviour
         if (player) //Player was destroyed
         {
             playerPosition = player.transform.position;
-            transform.position = new Vector3(playerPosition.x + offsetX, playerPosition.y + offsetY, -10f);
+            Vector3 newCameraPos = playerPosition;
+            if (playerPosition.y < 0) // if below middle of screen
+            {
+                newCameraPos.y += offsetY; // camera should be higher than usual by offset amount, otherwise, in middle of screen
+            }
+            newCameraPos.x += offsetX;
+            newCameraPos.z = -10f;
+            transform.position = Vector3.Lerp(transform.position, newCameraPos, smoothing * Time.deltaTime);
         }
     }
 }
