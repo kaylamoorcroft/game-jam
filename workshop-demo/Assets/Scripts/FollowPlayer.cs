590922b3acd5ce64335676b38cd6793d574fcf63
kaylamoorcroft <kayla.moorcroft@icloud.com>
2024-03-23 17:22:51 -0300

    Fixed camera movement

--- workshop-demo/Assets/Scripts/FollowPlayer.cs
+++ workshop-demo/Assets/Scripts/FollowPlayer.cs
@@ -8,7 +8,6 @@ public class FollowPlayer : MonoBehaviour
     [SerializeField] private float offsetX;
     [SerializeField] private float offsetY;
     [SerializeField] private float smoothing = 1f; // controls how smooth camera moves
-    private Vector2 playerPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -24,17 +23,9 @@ public class FollowPlayer : MonoBehaviour
     {
         if (player) //Player was destroyed
         {
-            playerPosition = player.transform.position;
-            Vector3 newCameraPos = playerPosition;
-            if (playerPosition.y < 0) // if below middle of screen
-            {
-                newCameraPos.y += offsetY; // camera should be higher than usual by offset amount, otherwise, in middle of screen
-            }
-            else if (playerPosition.y > 2.5) // if higher, move camera down
-            {
-                newCameraPos.y -= offsetY;
-            }
+            Vector3 newCameraPos = player.transform.position;
             newCameraPos.x += offsetX;
+            newCameraPos.y += offsetY;
             newCameraPos.z = -10f;
             transform.position = Vector3.Lerp(transform.position, newCameraPos, smoothing * Time.deltaTime);
         }
